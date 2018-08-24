19849ae3eeebbb119c163b665c10f73daf14f8c5
Dan Koe <thedankoe@gmail.com>
2018-08-24 08:58:11 -0700

    start

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Welcome text="Welcome to React" />
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+class Welcome extends Component {
+  render() {
+    const { text } = this.props;
+    return(
+      <h1 className="App-title">{text}</h1>
+    );
+  }
+}
+
+export default App;
